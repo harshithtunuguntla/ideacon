d2b07ccc4d3834396f5e71ff1386ce01527beb64
harshithtunuguntla <harshithtunuguntla@gmail.com>
2022-02-13 02:22:53 +0530

    Added static dir path

--- ideacon/settings.py
+++ ideacon/settings.py
@@ -122,9 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+import django_heroku
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = "/static/"
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
